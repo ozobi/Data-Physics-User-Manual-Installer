24289224abe517bef421786bb9bb8ecea6bc7fb8
ozobi <13084362+ozobi@users.noreply.github.com>
2019-02-28 18:17:28 +0100

    fix test script

--- install.ps1
+++ install.ps1
@@ -6,23 +6,33 @@ function PAUSE{
 try{
 	write-host "1/5 Installing Chocolatey..." -foregroundcolor Yellow
 	write-host 
-	Start-Process powershell.exe (Resolve-Path .\scripts\install_chocolatey.ps1) -Verb runAs | Wait-Process
+	$p1i = Resolve-Path $PSScriptRoot\scripts\install_chocolatey.ps1
+	$p1 = Start-Process -FilePath powershell.exe -ArgumentList "-file `"$($p1i.Path)`"" -Verb runAs -PassThru
+	$p1.WaitForExit()
 	
 	write-host "2/5 Installing dependencies..." -foregroundcolor Yellow
 	write-host 
-	Start-Process powershell.exe (Resolve-Path .\scripts\install_dependencies.ps1) -Verb runAs | Wait-Process
+	$p2i = Resolve-Path $PSScriptRoot\scripts\install_dependencies.ps1
+	$p2 = Start-Process -FilePath powershell.exe -ArgumentList "-file `"$($p2i.Path)`"" -Verb runAs -PassThru
+	$p2.WaitForExit()
 	
 	write-host "3/5 Updating MikTeX..." -foregroundcolor Yellow
 	write-host 
-	Start-Process powershell.exe (Resolve-Path .\scripts\update_miktex.ps1) -Verb runAs | Wait-Process
+	$p3i = Resolve-Path $PSScriptRoot\scripts\update_miktex.ps1
+	$p3 = Start-Process -FilePath powershell.exe -ArgumentList "-file `"$($p3i.Path)`"" -Verb runAs -PassThru
+	$p3.WaitForExit()
 	
 	write-host "4/5 Downloading repository..." -foregroundcolor Yellow
 	write-host 
-	Start-Process powershell.exe (Resolve-Path .\scripts\download_repository.ps1) -Verb runAs | Wait-Process
+	$p4i = Resolve-Path $PSScriptRoot\scripts\download_repository.ps1
+	$p4 = Start-Process -FilePath powershell.exe -ArgumentList "-file `"$($p4i.Path)`"" -Verb runAs -PassThru
+	$p4.WaitForExit()
 	
 	write-host "5/5 Preparing a test page..." -foregroundcolor Yellow
 	write-host 
-	Start-Process powershell.exe (Resolve-Path .\scripts\compile_test_page.ps1) -Verb runAs | Wait-Process
+	$p5i = Resolve-Path $PSScriptRoot\scripts\compile_test_page.ps1
+	$p5 = Start-Process -FilePath powershell.exe -ArgumentList "-file `"$($p5i.Path)`"" -Verb runAs -PassThru
+	$p5.WaitForExit()
 	
 	write-host "Done!" -foregroundcolor Green
 	write-host 
--- scripts/compile_test_page.ps1
+++ scripts/compile_test_page.ps1
@@ -1,4 +1,5 @@
-cd .\test
+Set-Location (Resolve-Path(($PSScriptRoot | Split-Path -Parent)+"/test"))
+
 pdflatex test.tex
 pdflatex test.tex
 Start-Process test.pdf
